ba1917a4abed3fb535f0e44310c06b7cf4534b3b
igor <igorbkzuki711@outlook.com>
2026-03-09 12:10:04 -0300

    fix: timezone configuration

--- src/Kernel.php
+++ src/Kernel.php
@@ -10,4 +10,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $timezone = $_SERVER['APP_TIMEZONE'] ?? $_ENV['APP_TIMEZONE'] ?? 'America/Fortaleza';
+        date_default_timezone_set($timezone);
+    }
 }
